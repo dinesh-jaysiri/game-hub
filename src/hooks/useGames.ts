a9c99edc4535fa0b381bf35855cdcaa6cedd00ff
dinesh-jaysiri <dineshjayasiri@Dineshs-Laptop.local>
2023-06-15 10:31:56 +0530

    Displaying critc score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,8 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms:{platform:Platform}[]
+  metacritic:number;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesRespons {
