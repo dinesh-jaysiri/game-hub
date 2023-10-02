1ed79d27a1a09f846b9ff2bb38d225281cd2c99c
Dinesh Jayasiri <ywdinesh95@gmail.com>
2023-10-02 11:52:06 +0530

    Build game attributes

--- src/entities/game.ts
+++ src/entities/game.ts
@@ -1,9 +1,13 @@
 import {Platform} from "./platform";
+import {Genre} from "./genre";
+import {Publisher} from "./publisher";
 
 export interface Game {
     id: number;
     name: string;
     slug: string;
+    publishers:Publisher[];
+    genres:Genre[];
     description_raw: string;
     background_image: string;
     metacritic: number;
