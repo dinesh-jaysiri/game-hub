63b6276cec2a492b826459e0e23756ae344aaead
Dinesh Jayasiri <ywdinesh95@gmail.com>
2023-12-30 18:07:43 +0530

    build game Trailer

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+
 import {useParams} from "react-router-dom";
 import useGame from "../hooks/useGame";
 import {Heading, Spinner} from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from '../components/GameTrailer';
 
 function GameDetailPage() {
     const {slug} = useParams();
@@ -19,6 +20,7 @@ function GameDetailPage() {
             <Heading>{game.name}</Heading>
             <ExpandableText>{game.description_raw}</ExpandableText>
             <GameAttributes game={game} />
+            <GameTrailer gameId={game.id} />
 
         </>
     );
