42c9b0fdab2ee91d118893d2caf7ee99d52b18af
Dinesh Jayasiri <ywdinesh95@gmail.com>
2023-12-31 14:02:17 +0530

    build game screenshots

---
+++ src/hooks/useScreenshots.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { Screenshot } from "../entities/screnshot";
+import APIClient from "../services/api-client";
+
+const useScreenshots = (gameId: number) => {
+  const apiclient = new APIClient<Screenshot>(`/games/${gameId}/screenshots`);
+
+  return useQuery({
+    queryKey: ["screenshots", gameId],
+    queryFn: apiclient.getAll,
+  });
+};
+
+export default useScreenshots;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -5,6 +5,7 @@ import {Heading, Spinner} from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from '../components/GameTrailer';
+import GameScreenshots from "../components/gameScreenshots";
 
 function GameDetailPage() {
     const {slug} = useParams();
@@ -21,6 +22,7 @@ function GameDetailPage() {
             <ExpandableText>{game.description_raw}</ExpandableText>
             <GameAttributes game={game} />
             <GameTrailer gameId={game.id} />
+            <GameScreenshots gameId={game.id} />
 
         </>
     );
