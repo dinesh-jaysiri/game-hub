56dc0b5c5788423754517cc93cfbdfc185952ae2
Dinesh Jayasiri <ywdinesh95@gmail.com>
2025-02-18 13:10:33 +0530

    bugfix

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -4,7 +4,7 @@ import { GridItem, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
-import GameScreenshots from "../components/gameScreenshots";
+import GameScreenshots from "../components/GameScreenshots";
 function GameDetailPage() {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
